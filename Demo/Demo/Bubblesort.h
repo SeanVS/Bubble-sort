0e8f0793d5e535f45dd15c63042b7ff2bd28ba91
Sean Van Stone <seantvan.stone@gmail.com>
2015-08-30 13:03:04 -0600

    Testing this

---
+++ Demo/Demo/Bubblesort.h
@@ -0,0 +1,23 @@
+#ifndef BUBBLESORT_H
+#define BUBBLESORT_H
+
+class SortableArray
+{
+private:
+	int *m_data;  //m_data will point to an array
+	int m_length; //m_length stores the length of that array
+
+	//SortableArray();
+
+public:
+	SortableArray(int length);
+
+	~SortableArray();
+
+	int& operator[](int index);
+
+	int getLength() { return m_length; }
+	void printElements();
+	void bubbleSort();
+};
+#endif
---
+++ Demo/Demo/Demo.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "Bubblesort.h"
+#include <iostream>
+#include <ctime>
+
+
+// This is just a demo, and you'll get errors as there are missing files.
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	SortableArray TestArray(10);
+	srand(static_cast<unsigned int>(time(0)));
+	for (int i = 0; i < (TestArray.getLength()); i++)
+	{
+		TestArray[i] = rand() % 30;
+	}
+
+	std::cout << "The elements of the array are randomly assigned: " << std::endl;
+	TestArray.printElements();
+
+	std::cout << "Then they are sorted into the right order: " << std::endl;
+	TestArray.bubbleSort();
+
+	TestArray.printElements();
+
+	return 0;
+
+}
